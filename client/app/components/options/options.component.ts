d674fd5409bb003b9850642745c69274b7e5e04f
Michael Chen W <michael.cw02@hotmail.com>
2017-09-05 09:41:04 -0600

    Merge with Andrey [Complete]

--- client/app/components/options/options.component.ts
+++ client/app/components/options/options.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 
 import { CommunicationService } from '../../services/communication.service'
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
