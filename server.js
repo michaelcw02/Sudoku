a4d4db4f82369255abd4daf11318da1990cb65aa
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-09-30 14:49:39 -0600

    Testing mlab 2

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const port         = process.env.PORT || 9090;
 const app          = express()
 
 mongoose.connect('mongodb://admin:admin@ds147454.mlab.com:47454/sudoku', {useMongoClient: true})
+
+//'mongodb://localhost/sudokus //for local deployment
 //mongodb://admin:admin@ds147454.mlab.com:47454/sudoku //for heroku
 
 app.use(morgan('dev'))
