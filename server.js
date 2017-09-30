de21566da3c8b4a53de15e549de41f623d8304b4
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-09-30 14:11:30 -0600

    Testing paths for heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api', (req, res) => {
 } )
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 } )
 
 
