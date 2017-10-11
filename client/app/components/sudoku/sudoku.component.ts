8d1840141139af2dd93d4cc83a2f6490b9850f97
Michael Chen W <michael.cw02@hotmail.com>
2017-10-11 16:33:29 -0600

    Fixed Small bug> Loading not going off while offline

--- client/app/components/sudoku/sudoku.component.ts
+++ client/app/components/sudoku/sudoku.component.ts
@@ -174,19 +174,28 @@ export class SudokuComponent implements OnInit {
 
   solve() {
     this.loading = true;
-    (!navigator.onLine) ? this.sudokuSolver.solve(this.sudoku)
-                        : this.sudokuService.getSolution(this.sudoku)
-                              .subscribe(
-                                res => {
-                                  this.loading = false
-                                  this.sudoku.load(res.grid)
-                                },
-                                err => {
-                                  this.loading = false
-                                  this.sudokuSolver.solve(this.sudoku)
-                                }
-                              )
+    (!navigator.onLine) ? this.localSolve()
+                        : this.serverSolve()
     //return this.sudokuSolver.solve(this.sudoku);
+  }
+
+  localSolve() {
+    this.loading = false;
+    this.sudokuSolver.solve(this.sudoku);
+  }
+
+  serverSolve() {
+    this.sudokuService.getSolution(this.sudoku)
+        .subscribe(
+          res => {
+            this.loading = false
+            this.sudoku.load(res.grid)
+          },
+          err => {
+            this.loading = false
+            this.sudokuSolver.solve(this.sudoku)
+          }
+        )
   }
   
   solveStepByStep(){
